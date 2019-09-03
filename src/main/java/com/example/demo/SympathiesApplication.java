76a69d1551019e938e7a21ef3384a2545622eeee
shumaikunkun <s1711520@gmail.com>
2019-09-03 14:57:07 +0900

    symponiesからsympathiesに

--- src/main/java/com/example/demo/SympathiesApplication.java
+++ src/main/java/com/example/demo/SympathiesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SymphoniesApplication {
+public class SympathiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SymphoniesApplication.class, args);
+		SpringApplication.run(SympathiesApplication.class, args);
 	}
 
 }
