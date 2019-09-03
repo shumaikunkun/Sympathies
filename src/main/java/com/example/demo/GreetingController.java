9b1ba3e3e5117bb5676c6d5ae714a994fd54ceba
shumaikunkun <s1711520@gmail.com>
2019-09-03 19:52:53 +0900

    yu-zameito

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -3,21 +3,27 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("path", "/greeting?name="+name);
+
+    @GetMapping("/")
+    public String hello(@ModelAttribute("inputUsr") InputUsr inputUsr) {
         return "hello";
     }
+
+    @PostMapping(path="/greeting")
+    public String greeting(@ModelAttribute("inputUsr") InputUsr inputUsr) {
+        return "greeting";
+    }
+
+
+
+
+
 
 }
