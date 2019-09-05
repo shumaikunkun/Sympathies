3bbd8d7442dcdbad61ad2d955a989a5c025aa2c2
shumaikunkun <s1711520@gmail.com>
2019-09-05 20:35:00 +0900

    test

--- src/main/java/com/example/demo/SympathiesApplication.java
+++ src/main/java/com/example/demo/SympathiesApplication.java
@@ -30,6 +30,7 @@ public class SympathiesApplication {
 			// save a couple of users
 			userRepo.save(new User("test@example.com", "Taro", "passwd", 100));
 			userRepo.save(new User("test2@example.com", "Hanako", "mypasswd", 50));
+			userRepo.save(new User("shuma@gmail.com", "秀馬", "shuma", 100));
 		};
 	}
 
@@ -41,11 +42,11 @@ public class SympathiesApplication {
 			goodsRepo.save(new Goods(1L, "過去問", "20XX年の期末試験の過去問です．", 10, "images/document_regulation.png"));
 			goodsRepo.save(new Goods(2L, "数学の解き方", "微分積分問題集の解法です．", 20, "images/math.png"));
 			goodsRepo.save(new Goods(3L, "英語 ドキュメント", "英語の参考文献です．", 40, "images/english.png"));
-			goodsRepo.save(new Goods(4L, "Test Item 01", "商品説明です．", 10, "images/noimage.png"));
-			goodsRepo.save(new Goods(5L, "Test Item 02", "商品説明です．", 20, "images/noimage.png"));
-			goodsRepo.save(new Goods(6L, "Test Item 03", "商品説明です．", 30, "images/noimage.png"));
-			goodsRepo.save(new Goods(7L, "Test Item 04", "商品説明です．", 40, "images/noimage.png"));
-			goodsRepo.save(new Goods(8L, "Test Item 05", "商品説明です．", 100, "images/noimage.png"));
+			goodsRepo.save(new Goods(4L, "Test Item 01", "商品説明です．", 10, "images/1.jpg"));
+			goodsRepo.save(new Goods(5L, "Test Item 02", "商品説明です．", 20, "images/2.jpg"));
+			goodsRepo.save(new Goods(6L, "Test Item 03", "商品説明です．", 30, "images/3.jpg"));
+			goodsRepo.save(new Goods(7L, "Test Item 04", "商品説明です．", 40, "images/4.jpg"));
+			goodsRepo.save(new Goods(8L, "Test Item 05", "商品説明です．", 100, "images/5.jpg"));
 
 			for (Goods item : goodsRepo.findAll()) {
 				log.info(item.toString());
